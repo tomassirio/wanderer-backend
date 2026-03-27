8c4df8bb08b7eda7458e4ea90a9994837838023b
tomassirio <tomassirio@gmail.com>
2026-03-27 12:20:22 +0100

    feat: Integrate Redis for JTI revocation checks in JWT authentication

---
+++ commons/src/main/java/com/tomassirio/wanderer/commons/security/revocation/RevokedTokenCache.java
@@ -0,0 +1,23 @@
+package com.tomassirio.wanderer.commons.security.revocation;
+
+/**
+ * Service for managing revoked JWT tokens using Redis.
+ */
+public interface RevokedTokenCache {
+
+    /**
+     * Revoke a token by adding its JTI to the blacklist.
+     *
+     * @param jti the JWT ID
+     * @param expiresInSeconds time until the token naturally expires
+     */
+    void revokeToken(String jti, long expiresInSeconds);
+
+    /**
+     * Check if a token is revoked.
+     *
+     * @param jti the JWT ID
+     * @return true if the token is revoked
+     */
+    boolean isTokenRevoked(String jti);
+}
